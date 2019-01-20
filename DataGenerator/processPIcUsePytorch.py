4e46d69f0ad9321ce3ac0243e45bc22192f2568c
ggqshr <942490944@qq.com>
2019-01-20 17:43:01 +0800

    添加了文档说明

--- DataGenerator/processPIcUsePytorch.py
+++ DataGenerator/processPIcUsePytorch.py
@@ -31,21 +31,21 @@ def trans_pic(file_dir, out_dir, max_iter=10, gen_test=False, test_scala=0.3, pi
         T.ToTensor()
     ])
 
-    if not os.path.exists(file_dir):
+    if not os.path.exists(file_dir):  # 检测原始图片的地址是否存在
         print("the data dir not exists!")
         return None
-    if not os.path.exists(out_dir):
+    if not os.path.exists(out_dir):  # 如果要输出的目标文件夹不存在，就创建一个
         os.mkdir(out_dir)
 
-    if os.path.isdir(file_dir):
-        image_data = ImageFolder(file_dir, transform=trans)  # 如果是文件夹
-        class_dict = {v: k for k, v in image_data.class_to_idx.items()}
-        for i in trange(max_iter):
-            target_dir = os.path.join(out_dir, "totrain")
-            if gen_test and i + 1 > max_iter * (1 - test_scala):  # 生成测试集
-                target_dir = os.path.join(out_dir, "totest")
+    if os.path.isdir(file_dir):  # 如果是文件夹，就使用ImageFolder
+        image_data = ImageFolder(file_dir, transform=trans)
+        class_dict = {v: k for k, v in image_data.class_to_idx.items()}  # 将class编号和class对调
+        for i in trange(max_iter):  # 循环创建新的图片
+            target_dir = os.path.join(out_dir, "totrain")  # 默认是创建训练的图片
+            if gen_test and i + 1 > max_iter * (1 - test_scala):  # 如果标记为真，且达到了要生成测试的比例
+                target_dir = os.path.join(out_dir, "totest")  # 构建测试文件夹地址
 
-            if not os.path.exists(target_dir):
+            if not os.path.exists(target_dir):  # 检查输出地址是否存在
                 os.mkdir(target_dir)
 
             for d, l in image_data:
@@ -56,7 +56,7 @@ def trans_pic(file_dir, out_dir, max_iter=10, gen_test=False, test_scala=0.3, pi
                 file = os.path.join(target_d, file_name)
                 save_image(d, file)
 
-    else:
+    else:  # 如果是单个文件，就使用PIL库的读取单个的图片文件
         image_data = Image.open(file_dir)
         for i in trange(max_iter):
             porcess_pic = trans(image_data)  # 将图片变换
